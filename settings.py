5f5fbcb4987092f01bf450ba8a88b387607437d8
Benedikt <benedikt.putz@stud.uni-regensburg.de>
2018-03-16 11:01:10 +0100

    also find settings file when working directory is not the code directory

--- settings.py
+++ settings.py
@@ -1,9 +1,10 @@
 import configparser
 import sys
 import types
+import os
 
 config = configparser.ConfigParser()
-config.read('settings.ini')
+config.read(os.path.dirname(os.path.realpath(__file__)) + '/settings.ini')
 
 class settings(types.ModuleType):
     def __getitem__(self, key):
