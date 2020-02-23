50360bdc8fa60ccf1a49b0f09a7f1fb9d1c821e8
sigma67 <benedikt.putz@wiwi.uni-regensburg.de>
2020-02-23 19:34:55 +0100

    Add playlist updating and improve song matching

--- YouTube.py
+++ YouTube.py
@@ -18,8 +18,16 @@ class YTMusicTransfer:
     def get_best_fit_song_id(self, results, song):
         match_score = {}
         for res in results:
-            compare = res['artist'] + ' ' + res['title']
-            match_score[res['videoId']] = difflib.SequenceMatcher(a=song.lower(), b=compare.lower()).ratio()
+            if res['resultType'] not in ['song', 'video']:
+                continue
+
+            scores = [difflib.SequenceMatcher(a=res['artist'].lower(), b=song['artist'].lower()).ratio(),
+                      difflib.SequenceMatcher(a=res['title'].lower(), b=song['name'].lower()).ratio()]
+
+            if res['resultType'] == 'song':
+                scores.append(difflib.SequenceMatcher(a=res['album'].lower(), b=song['album'].lower()).ratio())
+
+            match_score[res['videoId']] = sum(scores) / len(scores)
 
         return max(match_score, key=match_score.get)
 
@@ -28,13 +36,14 @@ class YTMusicTransfer:
         songs = list(tracks)
         notFound = list()
         for i, song in enumerate(songs):
-            song = song.replace(" &", "")
-            result = self.api.search(song)
+            query = song['artist'] + ' ' + song['name']
+            query = query.replace(" &", "")
+            result = self.api.search(query)
             if len(result) == 0:
-                notFound.append(song)
+                notFound.append(query)
             else:
                 videoIds.append(self.get_best_fit_song_id(result, song))
-            if i % 10 == 0:
+            if i > 0 and i % 10 == 0:
                 print(str(i) + ' searched')
 
         return videoIds, notFound
@@ -42,18 +51,31 @@ class YTMusicTransfer:
     def add_songs(self, playlist, tracks):
         videoIds, notFound = self.search_songs(tracks)
 
-        for videoId in videoIds:
-            self.api.add_playlist_item(playlist, videoId)
+        self.api.add_playlist_items(playlist, videoIds)
 
         with open(path + 'noresults_youtube.txt', 'w', encoding="utf-8") as f:
             f.write("\n".join(notFound))
             f.close()
 
+    def get_playlist_id(self, name):
+        result = {}
+        try:
+            result = self.api.search(name, 'playlists')[0]
+        except Exception as e:
+            print(e)
+
+        return result['browseId'][2:]
+
+    def remove_songs(self, playlistId):
+        items = self.api.get_playlist_items(playlistId)
+        if len(items) > 0:
+            self.api.remove_playlist_items(playlistId, items)
+
 
 def get_args():
     parser = argparse.ArgumentParser(description='Transfer spotify playlist to YouTube Music.')
     parser.add_argument("playlist", type=str, help="Provide a playlist Spotify link. Alternatively, provide a text file (one song per line)")
-   #parser.add_argument("-u", "--update", type=str, help="Delete all entries in the provided Google Play Music playlist and update the playlist with entries from the Spotify playlist.")
+    parser.add_argument("-u", "--update", type=str, help="Delete all entries in the provided Google Play Music playlist and update the playlist with entries from the Spotify playlist.")
     parser.add_argument("-n", "--name", type=str, help="Provide a name for the YouTube Music playlist. Default: Spotify playlist name")
     parser.add_argument("-i", "--info", type=str, help="Provide description information for the YouTube Music Playlist. Default: Spotify playlist description")
     parser.add_argument("-d", "--date", action='store_true', help="Append the current date to the playlist name")
@@ -77,9 +99,16 @@ def main():
     name = args.name + date if args.name else playlist['name'] + date
     info = playlist['description'] if (args.info is None) else args.info
     ytmusic = YTMusicTransfer()
-    playlistId = ytmusic.create_playlist(name, info, 'PUBLIC' if args.public else 'PRIVATE')
-    ytmusic.add_songs(playlistId, playlist['tracks'])
-    print("Success: created playlist \"" + name + "\"")
+
+    if args.update:
+        playlistId = ytmusic.get_playlist_id(args.update)
+        ytmusic.remove_songs(playlistId)
+        ytmusic.add_songs(playlistId, playlist['tracks'])
+
+    else:
+        playlistId = ytmusic.create_playlist(name, info, 'PUBLIC' if args.public else 'PRIVATE')
+        ytmusic.add_songs(playlistId, playlist['tracks'])
+        print("Success: created playlist \"" + name + "\"")
 
 
 if __name__ == "__main__":
