44262f60efe1438d8e5954ed8fb87b5ede2b130f
sigma67 <benedikt.putz@wiwi.uni-regensburg.de>
2020-02-23 19:01:14 +0100

    Fix Spotify playlist result building

--- SpotifyExport.py
+++ SpotifyExport.py
@@ -8,29 +8,34 @@ class Spotify:
         client_credentials_manager = SpotifyClientCredentials(client_id=conf['client_id'], client_secret=conf['client_secret'])
         self.api = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
+    def build_results(self, tracks):
+        results = []
+        for track in tracks['items']:
+            if track['track'] is not None:
+                results.append({
+                    'artist': ' '.join([artist['name'] for artist in track['track']['artists']]),
+                    'name': track['track']['name'],
+                    'album': track['track']['album']['name']
+                })
+
+        return results
+
     def getSpotifyPlaylist(self, url):
         url_parts = url.split('/')
         playlistId = url_parts[4].split('?')[0]
         if len(playlistId) != 22:
             raise Exception('Bad playlist id: ' + playlistId)
 
-        tracks = []
         results = self.api.playlist(playlistId)
         name = results['name']
-        for track in results['tracks']["items"]:
-            if track['track'] is not None:
-                tracks.append({
-                    'artist': ' '.join([artist['name'] for artist in track['track']['artists']]),
-                    'name': track['track']['name'],
-                    'album': track['track']['album']['name']
-                })
+        tracks = self.build_results(results['tracks'])
 
         count = 1
         more = len(results['tracks']['items']) == 100
         while more:
             items = self.api.playlist_tracks(playlistId, offset = count * 100, limit=100)
             print('requested from ' + str(count * 100))
-            tracks += list(track['track']['artists'][0]['name'] + ' ' + track['track']['name'] for track in items["items"] if track['track'] is not None)
+            tracks += self.build_results(items)
             more = len(items["items"]) == 100
             count = count + 1
 
