sigma67/spotify_to_ytmusic

commit a296565384dfc43746f2eed31135e1ca4318a442

use format string and refactor spotify search loop to show progress.
This makes it easier to visually confirm  that the all playlist tracks are being processed.

```
$  python YouTube.py https://open.spotify.com/playlist/0yasezcPZp9GvDpiIonDPv
Spotify tracks: 100/1587
Spotify tracks: 200/1587
...
Spotify tracks: 1400/1587
Spotify tracks: 1500/1587
Spotify tracks: 1587/1587
YouTube tracks: 10/1587
YouTube tracks: 20/1587
YouTube tracks: 30/1587
YouTube tracks: 40/1587
...
```

Format strings required Python 3.6+ so update the README.md.

Also added a newline to the "noresults" file so that the shell prompt is on a new line after `cat`ing the file.

SpotifyExport.py
@@ -17,16 +17,15 @@ class Spotify:
 
         results = self.api.playlist(playlistId)
         name = results['name']
-        tracks = build_results(results['tracks']['items'])
+        total = int(results['tracks']['total'])
+        tracks = list()
 
-        count = 1
-        more = len(results['tracks']['items']) == 100
-        while more:
-            more_tracks = self.api.playlist_items(playlistId, offset = count * 100, limit=100)
-            print('requested from ' + str(count * 100))
-            tracks += build_results(more_tracks['items'])
-            more = len(more_tracks["items"]) == 100
-            count = count + 1
+        count = 0
+        while count < total:
+            req = self.api.playlist_items(playlistId, offset=count, limit=100)
+            tracks += build_results(req['items'])
+            print(f"Spotify tracks: {len(tracks)}/{total}")
+            count = count + 100
 
         return {'tracks': tracks, 'name': name, 'description': html.unescape(results['description'])}
 

YouTube.py
@@ -78,10 +78,11 @@ class YTMusicTransfer:
                     videoIds.append(targetSong)
 
             if i > 0 and i % 10 == 0:
-                print(str(i) + ' searched')
+                print(f"YouTube tracks: {i}/{len(songs)}")
 
         with open(path + 'noresults_youtube.txt', 'w', encoding="utf-8") as f:
             f.write("\n".join(notFound))
+            f.write("\n")
             f.close()
 
         return videoIds

commit 773fee72e65f2d866ddfb67137f761352b4f2248

Fix double request bug introduced by a296565 in #17

SpotifyExport.py
@@ -18,14 +18,15 @@ class Spotify:
         results = self.api.playlist(playlistId)
         name = results['name']
         total = int(results['tracks']['total'])
-        tracks = list()
+        tracks = build_results(results['tracks']['items'])
+        count = len(tracks)
+        print(f"Spotify tracks: {count}/{total}")
 
-        count = 0
         while count < total:
-            req = self.api.playlist_items(playlistId, offset=count, limit=100)
-            tracks += build_results(req['items'])
-            print(f"Spotify tracks: {len(tracks)}/{total}")
+            more_tracks = self.api.playlist_items(playlistId, offset=count, limit=100)
+            tracks += build_results(more_tracks['items'])
             count = count + 100
+            print(f"Spotify tracks: {count}/{total}")
 
         return {'tracks': tracks, 'name': name, 'description': html.unescape(results['description'])}
 