0314dacc0d433737e11eef4815dd5d2bf2d71bb6
Tim Odermatt <91256797+Tim-Odermatt@users.noreply.github.com>
2024-06-17 20:00:25 +0200

    Update controllers.py
    Added guglielmobartelloni:playlist-to-like to the newer version of spotify_to_ytmusic for the all argument

--- spotify_to_ytmusic/controllers.py
+++ spotify_to_ytmusic/controllers.py
@@ -46,6 +46,10 @@ def all(args):
                 "PUBLIC" if p["public"] else "PRIVATE",
                 videoIds,
             )
+            if args.like:
+                for id in videoIds:
+                    ytmusic.rate_song(id, "LIKE")
+                    print(id)
             _print_success(p["name"], playlist_id)
         except Exception as ex:
             print(f"Could not transfer playlist {p['name']}. {str(ex)}")
